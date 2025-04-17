f2ebb4e3f2984098d58edd0ebaac91e551da573d
Irina Lisovskaja <179472627+irinal2025@users.noreply.github.com>
2025-04-17 16:40:35 +0300

    Refactor theme toggle functionality and ensure dark theme initialization occurs only if toggle exists

--- js/script.js
+++ js/script.js
@@ -7,26 +7,30 @@ $(document).ready(function () {
 
 // Vanilla JavaScript for the menu toggle functionality
 document.addEventListener('DOMContentLoaded', () => {
+
+  // Menu toggle
   const menuToggle = document.getElementById('menu-toggle');
   const menu = document.getElementById('menu');
 
   menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active');
   });
-});
 
-const toggle = document.getElementById('theme-switch');
-if (toggle) {
-  toggle.addEventListener('change', () => {
-    document.body.classList.toggle('dark-theme', toggle.checked);
-    localStorage.setItem('theme', toggle.checked ? 'dark' : 'light');
-  });
 
-  // Dark Theme Initialization
-  window.addEventListener('DOMContentLoaded', () => {
+  // Theme toggle
+  const toggle = document.getElementById('theme-switch');
+  if (toggle) {
+    toggle.addEventListener('change', () => {
+      document.body.classList.toggle('dark-theme', toggle.checked);
+      localStorage.setItem('theme', toggle.checked ? 'dark' : 'light');
+    });
+
+    // Dark Theme Initialization
     if (localStorage.getItem('theme') === 'dark') {
       document.body.classList.add('dark-theme');
       toggle.checked = true;
     }
-  });
-}
+  }
+
+});
+
